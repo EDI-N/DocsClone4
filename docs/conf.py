be42e0c71de901105aff58b73fc8dfdb02b1196e
Kolya Bezgin <bezginn@edi-n.com>
2020-01-23 18:34:34 +0200

    save

--- docs/conf.py
+++ docs/conf.py
@@ -132,6 +132,7 @@ html_theme = 'sphinx_rtd_theme'
 def setup(app):
     app.add_stylesheet('theme_overrides.css')
     app.add_javascript('galaga.js')
+    app.add_javascript('oktava.js')
 
 # А это то, что я нагородил (не используй это):
 # import os
