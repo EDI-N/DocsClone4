aec234e644ceb95cce30817d805945626271c0a5
Kolya Bezgin <bezginn@edi-n.com>
2020-01-23 17:47:17 +0200

    what_is_your_problem?

--- docs/_static/okta.js
+++ docs/_static/okta.js
@@ -1,7 +1,7 @@
 // Uses okta-signin-widget version 2.13.0
 
-var widget = new OktaSignIn({
-    baseUrl: "{{yourOktaDomain}}",
+/* var widget = new OktaSignIn({
+    baseUrl: "{{dev-366824.okta.com}}",
     logo: "https://wiki.edi-n.com/uk/latest/_static/logo_edin2.gif",
     i18n: {
       // Overriding English properties
@@ -24,4 +24,21 @@ var widget = new OktaSignIn({
   
   widget.renderEl({
     el: "#widget-container"
+  }); */
+
+<div id="widget-container"></div>
+
+<script>
+  var signIn = new OktaSignIn({baseUrl: 'https://${dev-366824.okta.com}'});
+  signIn.renderEl({
+    el: '#widget-container'
+  }, function success(res) {
+    if (res.status === 'SUCCESS') {
+      console.log('Do something with this sessionToken', res.session.token);
+    } else {
+    // The user can be in another authentication state that requires further action.
+    // For more information about these states, see:
+    //   https://github.com/okta/okta-signin-widget#rendereloptions-success-error
+    }
   });
+</script>
